0c3f66e2ab8db2537d0a2f58f895d8781a2cad36
nishant11122004 <nishant.17232@sakec.ac.in>
2024-08-05 14:41:56 +0530

    jenkins.java

---
+++ jenkins.java
@@ -0,0 +1,13 @@
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        // Declare two variables
+        int num1 = 5;
+        int num2 = 10;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is " + sum);
+    }
+}
